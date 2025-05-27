7b7ee5f63640ed59535ad3d5c6483ef8eaef110e
Taroonn <koyiladataroon.23.cse@anits.edu.in>
2025-05-27 17:25:02 +0530

    Update rps.js

--- rps.js
+++ rps.js
@@ -1,1 +1,20 @@
 
+function play(userChoice) {
+  const choices = ['rock', 'paper', 'scissors'];
+  const computer = choices[Math.floor(Math.random() * 3)];
+
+  let result = "";
+  if (userChoice === computer) {
+    result = "It's a tie!";
+  } else if (
+    (userChoice === 'rock' && computer === 'scissors') ||
+    (userChoice === 'paper' && computer === 'rock') ||
+    (userChoice === 'scissors' && computer === 'paper')
+  ) {
+    result = You win! Computer chose ${computer};
+  } else {
+    result = You lose! Computer chose ${computer};
+  }
+
+  document.getElementById('result').innerText = result;
+}
